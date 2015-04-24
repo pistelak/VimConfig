cfaa158ff3a6be02da389359bf6959190d215f2a
Radek Pištělák <radek.pistelak@gmail.com>
2015-04-24 16:22:49 +0200

    template

---
+++ template/=template=.c
@@ -0,0 +1,19 @@
+//  
+//  %FFILE%
+//  Nazev projektu
+// 
+//  Created by Radek Pistelak on %DAY%.%MONTH%.%YEAR%. 
+//  Copyright (c) %YEAR% Radek Pistelak. All rights reserved. 
+//
+//  Email: xpiste04@stud.fit.vutbr.cz
+//  Compiler: Apple LLVM version 6.0 (clang-600.0.54) (based on LLVM 3.5svn)
+//  Last modified date: 
+// 
+
+#include "%FILE%.h"
+
+
+%HERE%
+
+//  End of file: %FFILE%
+
---
+++ template/=template=.h
@@ -0,0 +1,21 @@
+//  
+//  %FFILE%
+//  Nazev projektu
+// 
+//  Created by Radek Pistelak on %DAY%.%MONTH%.%YEAR%. 
+//  Copyright (c) %YEAR% Radek Pistelak. All rights reserved. 
+//
+//  Email: xpiste04@stud.fit.vutbr.cz
+//  Compiler: Apple LLVM version 6.0 (clang-600.0.54) (based on LLVM 3.5svn)
+//  Last modified date: 
+// 
+
+#ifndef %GUARD%
+#define %GUARD%
+
+%HERE%
+
+#endif /* !%GUARD% */
+
+//  End of file: %FFILE%
+
