09c0cc5f3eac17129da7199ba7465492b7c3b79a
Radek Pistelak <radek.pistelak@gmail.com>
2025-05-26 14:30:06 +0200

    Improve navigation panel

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,42 @@
 -- file navigation - neo-tree
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = "Neotree",  
+  keys = {
+    { "<C-n>", function()
+        require("neo-tree.command").execute({
+          toggle = true,
+          dir = vim.loop.cwd(), 
+        })
+      end,
+      desc = "Toggle Neo-tree"
+    }
+  },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-  end
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      enable_git_status = true,
+      enable_diagnostics = true,
+      window = {
+        position = "left",
+        width = 45,
+        mappings = {
+          ["<space>"] = "none", 
+        },
+      },
+      filesystem = {
+        follow_current_file = {
+          enabled = true,
+        },
+        hijack_netrw_behavior = "open_default",
+        use_libuv_file_watcher = true,
+      },
+    })
+  end,
 }
